4c3f6759c4f1af2aff3c85b83539652334fb1f2b
DaYoung Jung <dayoung.jung0102@gmail.com>
2026-01-29 23:24:53 +0900

    Import pandas and load server data from CSV
    Added functionality to read and display server data from a CSV file.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖 서버 실시간 이상 탐지 대시보드 🤖')
 
 st.info('이 앱은 머신러닝 모델을 활용하여 실시간으로 서버 상태를 모니터링할 수 있게 시각화한 대시보드를 제공합니다')
+
+df = pd.read_csv('https://raw.githubusercontent.com/roundy00/keroro-machinelearning/refs/heads/master/Server-Machine-Dataset-main/processed_csv/machine-1-1/machine-1-1_test.csv')
+df
