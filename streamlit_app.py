bf4b4c6e476b15536590bc6dd8ff6327169b5e37
DaYoung Jung <dayoung.jung0102@gmail.com>
2026-01-29 23:26:19 +0900

    Implement expander for displaying raw data
    Added an expander to display raw data from CSV.

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ st.title('🤖 서버 실시간 이상 탐지 대시보드 🤖')
 
 st.info('이 앱은 머신러닝 모델을 활용하여 실시간으로 서버 상태를 모니터링할 수 있게 시각화한 대시보드를 제공합니다')
 
-df = pd.read_csv('https://raw.githubusercontent.com/roundy00/keroro-machinelearning/refs/heads/master/Server-Machine-Dataset-main/processed_csv/machine-1-1/machine-1-1_test.csv')
-df
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/roundy00/keroro-machinelearning/refs/heads/master/Server-Machine-Dataset-main/processed_csv/machine-1-1/machine-1-1_test.csv')
+  df
